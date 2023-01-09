828065f61ec4af1d0bd16721765b79a2923858cd
Ashwin vemula <ashwin.vemula@cawstudios.com>
2023-01-09 15:48:42 +0530

    lts

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ function App() {
       left: 50, //50% of the image relative size from left
     },
   ]);
-  const [zoom, setZoom] = useState("");
+  const [zoom, setZoom] = useState(1);
+  const [showDropDown, setShowDropdown] = useState(false);
+  const [activeNumber, setActiveNumber] = useState(0);
   // const [parent, setParent] = useState({});
   let parent;
   let dragCoords = {
@@ -20,51 +22,74 @@ function App() {
     y: 0,
   };
 
-  // console.log(markers, zoom);
-
-  const marker = ({ itemNumber }) => (
-    <div
-      style={{
-        backgroundColor: "#000",
-        color: "#fff",
-        height: "20px",
-        width: "20px",
-        borderRadius: "50%",
-      }}
-      draggable="true"
-      onDragStart={({ target }) => {
-        const data = target.getBoundingClientRect();
-        console.log({ data }, "start");
-        // debugger;
-        // setParent({
-        //   x: target.parentElement.parentElement.offsetWidth,
-        //   y: target.parentElement.parentElement.offsetHeight,
-        //   item: +target.innerHTML,
-        // });
-        parent = {
-          x: target.parentElement.parentElement.offsetWidth,
-          y: target.parentElement.parentElement.offsetHeight,
-          item: +target.innerHTML,
-        };
-      }}
-      onDragEnd={({ target }) => {
-        const data = target.getBoundingClientRect();
-        console.log({ data }, "end");
-        // debugger;
-      }}
-    >
-      {itemNumber}
-    </div>
-  );
+  const marker = ({ itemNumber }) => {
+    return (
+      <div
+        style={{
+          backgroundColor: "#000",
+          color: "#fff",
+          height: "20px",
+          width: "20px",
+          borderRadius: "50%",
+          // position: "relative",
+        }}
+        draggable="true"
+        onDragStart={({ target }) => {
+          const data = target.getBoundingClientRect();
+          console.log({ data }, "start");
+          parent = {
+            x: target.parentElement.parentElement.offsetWidth,
+            y: target.parentElement.parentElement.offsetHeight,
+            item: +target.textContent,
+          };
+        }}
+        onDragEnd={({ target }) => {
+          const data = target.getBoundingClientRect();
+          console.log({ data }, "end");
+        }}
+        onClick={() => {
+          setShowDropdown((prev) => !prev);
+          setActiveNumber(itemNumber);
+        }}
+      >
+        <div>{itemNumber}</div>
+        {showDropDown && activeNumber === itemNumber && (
+          <div
+            style={{
+              position: "absolute",
+            }}
+          >
+            <select onChange={() => setShowDropdown(false)}>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+            </select>
+          </div>
+        )}
+      </div>
+    );
+  };
 
   return (
-    <div>
-      <select onChange={({ target }) => setZoom(target.value)}>
-        <option value="10">10%</option>
-        <option value="20">20%</option>
-        <option value="30">30%</option>
-        <option value="70">70%</option>
-      </select>
+    <div
+      style={{
+        padding: "40px",
+      }}
+    >
+      <div
+        style={{
+          marginBottom: "20px",
+        }}
+      >
+        Information: click on the element to see the dropdown, and after
+        selecting a value, the dropdown will close.
+      </div>
+      <div>
+        <div>Zoom here</div>
+        <button onClick={() => setZoom(zoom + 0.1)}>+</button>
+        <button onClick={() => setZoom(zoom - 0.1)}>-</button>
+      </div>
       <div
         style={{
           display: "flex",
@@ -72,51 +97,53 @@ function App() {
       >
         <div
           style={{
-            width: "40%",
+            width: "800px",
+            height: "400px",
+            overflow: "scroll",
           }}
         >
-          jvkdfvdk
-        </div>
-        <div
-          className="App"
-          onDrop={({ target }) => {
-            const data = target.getBoundingClientRect();
-            // debugger;
-            let yo = markers;
-            const { x, y, item } = parent;
+          <div
+            className="App"
+            style={{
+              transform: `scale(${zoom})`,
+              transformOrigin: "0 0",
+            }}
+            onDrop={({ target }) => {
+              const data = target.getBoundingClientRect();
+              // debugger;
+              let yo = markers;
+              const { x, y, item } = parent;
 
-            const x1px = x / 100;
-            const y1px = y / 100;
-            const sib =
-              target.parentElement.parentElement.previousElementSibling.getBoundingClientRect()
-                .width;
-            // debugger;
-            console.log(sib);
+              const x1px = x / 100;
+              const y1px = y / 100;
 
-            yo.splice(item, 1, {
-              left: (dragCoords.x - sib) / x1px,
-              top: dragCoords.y / y1px - 2.5,
-            });
-            setMarkers([...yo]);
-            console.log({ yo, dragCoords });
-          }}
-          onDragOver={(e) => {
-            e.preventDefault();
+              const sib = 40;
+              // target.parentElement.parentElement.previousElementSibling.getBoundingClientRect()
+              //   .width;
 
-            dragCoords = {
-              x: e.clientX,
-              y: e.clientY,
-            };
-            // console.log(e);
-            // debugger;
-          }}
-        >
-          <ImageMarker
-            src={Image}
-            markers={markers}
-            onAddMarker={(marker) => setMarkers([...markers, marker])}
-            markerComponent={marker}
-          />
+              yo.splice(item, 1, {
+                left: (dragCoords.x - sib) / x1px,
+                top: dragCoords.y / y1px - 27.5,
+              });
+              setMarkers([...yo]);
+              console.log({ yo, parent, dragCoords, x1px, y1px });
+            }}
+            onDragOver={(e) => {
+              e.preventDefault();
+
+              dragCoords = {
+                x: e.clientX,
+                y: e.clientY,
+              };
+            }}
+          >
+            <ImageMarker
+              src={Image}
+              markers={markers}
+              onAddMarker={(marker) => setMarkers([...markers, marker])}
+              markerComponent={marker}
+            />
+          </div>
         </div>
 
         {/* <div
